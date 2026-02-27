55ffce2b82d6eb3b71a5b9fc79db32c3c7330d82
lucaslopescunha <lucaslopescunha@gmail.com>
2026-02-27 13:34:14 -0300

    First commit using interval observable.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class AppComponent implements OnInit {
   protected readonly title = signal('rxjs-app');
+  private desttroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+/** You can use a simple function here because you're only using "next"
+    interval(1000).subscribe({
+      next: (val) => console.log(val)
+    }); */
+    const subscription = interval(1000).subscribe(
+      (val) => console.log(val)
+    );
+
+    this.desttroyRef.onDestroy(() => {
+      /**
+       * Cleaning up the subscription
+       */
+      subscription.unsubscribe();
+    });
+  }
 }
